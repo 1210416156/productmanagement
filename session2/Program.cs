4b3ba3dbaeb5c0a67e2c6fddfd9c12b0cda48d92
tarunsai <tarunsai336@gmail.com>
2020-07-15 20:39:34 +0530

    initial commit

---
+++ session2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Refrigirator refrigirator = new Refrigirator();
+            refrigirator.SetRefrigiratorData(1001,"X1", "LG",20000,120, 5,"Single Door");
+            refrigirator.Display();
+
+           
+        }
+
+        static void DisplayProduct(Product product)
+        {
+            if (product.GetType() == typeof(Refrigirator))
+                ((Refrigirator)product).Display();
+
+        }
+    }
+}
---
+++ session2/Refrigirator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session2
+{
+    class Refrigirator : Product
+    {
+        int capacity;
+        int rating;
+        string doors;
+
+        internal Refrigirator()
+        {
+            Console.WriteLine("Refrigirator Default Constructor Called");
+        }
+
+        internal Refrigirator(int productId, string model, string brand, int price,
+            int capacity, int rating, string doors) : base(productId, model, brand, price) 
+        {
+            Console.WriteLine("Refrigirator Parameterized Constructor Called");
+            this.capacity = capacity;
+            this.rating = rating;
+            this.doors = doors;
+        }
+
+        internal void SetRefrigiratorData(int productId, string model, string brand, int price,
+            int capacity, int rating, string doors)
+        {
+            base.SetProductData(productId, model, brand, price);
+            this.capacity = capacity;
+            this.rating = rating;
+            this.doors = doors;
+        }
+
+        internal override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Capacity : {capacity} Rating : {rating} DisplayGlass : {doors}");
+
+        }
+    }
+}
